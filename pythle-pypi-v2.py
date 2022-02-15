a0dc4444ca65df4b067d37cf0c006a6b04b6e6f2
ezrashare21 <79548308+ezrashare21@users.noreply.github.com>
2022-02-15 11:34:07 -0800

    Update pythle-pypi-v2.py

--- pythle-pypi-v2.py
+++ pythle-pypi-v2.py
@@ -2,14 +2,41 @@
 
 #pythle
 
-from termcolor import colored
-import random, sys
+import random, sys, os, re
+
+RESET_RE = '\033\[0m'
+RESET = '\033[0m'
+COLORS = dict(
+        list(zip([
+            'grey',
+            'red',
+            'green',
+            'yellow',
+            'blue',
+            'magenta',
+            'cyan',
+            'white',
+            ],
+            list(range(30, 38))
+            ))
+        )
+
+COLORS_RE = '\033\[(?:%s)m' % '|'.join(['%d' % v for v in COLORS.values()])
+
+def colored(text, color=None):
+    if os.getenv('ANSI_COLORS_DISABLED') is None:
+        fmt_str = '\033[%dm%s'
+        if color is not None:
+            text = re.sub(COLORS_RE + '(.*?)' + RESET_RE, r'\1', text)
+            text = fmt_str % (COLORS[color], text)
+        return text + RESET
+    else:
+        return text
 
 def out(stri):
     sys.stdout.write(stri)
     sys.stdout.flush()
 
-out("\n" * 3)
 
 wfile = open(__file__,"a")
 rfile = open(__file__,"r")
@@ -23,6 +50,8 @@ if data == "newgame":
     afdata = input("pythle has no words yet. can you supply some? (text file)")
     wfile.write("\n#" + open(afdata).read())
 else:
+    out("-" * 10 + " PYTHLE v1 " + "-" * 10)
+    out("\n" * 3)
     words = data[slice(1,-1)].split(",")
     word = words[random.randint(0,len(words)-1)]
     inp = ""
@@ -30,7 +59,7 @@ else:
     while x < 6:
         inp = ""
         x += 1
-        while len(inp) != 5 and (not inp in words):
+        while len(inp) != 5 or (not inp in words):
             inp = input("Attempt " + str(x) + ":")
             out("\n")
             if len(inp) != 5: out("Word needs to have a length of 5.\n")
@@ -50,11 +79,11 @@ else:
                 out(char)
         
         if al == 5:
-            print("\n\nYou Win! Word Was: " + word)
+            print("\n\nYou Win! Word Was: " + colored(word,"green"))
             exit()
         
         print("\n\n------------\n")
-    print("You didn't guess the right word. Good luck next time! Word Was: " + word)
+    print("You didn't guess the right word. Good luck next time! Word Was: " + colored(word,"red"))
 
 exit()
 newgame
